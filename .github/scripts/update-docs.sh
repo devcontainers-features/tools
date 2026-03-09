92c2a4567286559908f4d43b7e27dbb165c82ba9
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-03-09 11:44:13 +0400

    fix(ci): push docs directly to main with skip ci to prevent release loop

--- .github/scripts/update-docs.sh
+++ .github/scripts/update-docs.sh
@@ -3,17 +3,10 @@ set -e
 
 git config --global user.email github-actions[bot]@users.noreply.github.com
 git config --global user.name github-actions[bot]
-git config pull.rebase false
-
-branch=automated-documentation-update-$GITHUB_RUN_ID
-git checkout -b $branch
-
-message='Automated documentation update'
 
 git add */**/README.md
-git commit -m 'Automated documentation update [skip ci]' || export NO_UPDATES=true
+git commit -m 'docs: update auto-generated documentation [skip ci]' || export NO_UPDATES=true
 
 if [ "$NO_UPDATES" != "true" ] ; then
-    git push origin "$branch"
-    gh pr create --title "$message" --body "$message"
+    git push origin main
 fi
